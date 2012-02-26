6627533adc4024b4167aaac43c25b78fc362ee8f
Bertrand Paquet <bpaquet@octo.com>
2012-02-26 22:01:24 +0100

    Fix warp plugin when calling from a ruby binary

--- common/ruby/rbenv-plugin/etc/rbenv.d/exec/warp.bash
+++ common/ruby/rbenv-plugin/etc/rbenv.d/exec/warp.bash
@@ -1,4 +1,9 @@
-export LD_LIBRARY_PATH="$RBENV_BIN_PATH/../lib:$LD_LIBRARY_PATH"
-if [ -f "$RBENV_BIN_PATH/.rubylib" ]; then
-  export RUBYLIB="`cat $RBENV_BIN_PATH/.rubylib`:$RUBYLIB"
+BIN_PATH=$RBENV_BIN_PATH
+if [ ! -d $BIN_PATH/lib ]; then
+  BIN_PATH=`cd $BIN_PATH/../../../bin && pwd`
+fi
+
+export LD_LIBRARY_PATH="$BIN_PATH/../lib:$LD_LIBRARY_PATH"
+if [ -f "$BIN_PATH/.rubylib" ]; then
+  export RUBYLIB="`cat $BIN_PATH/.rubylib`:$RUBYLIB"
 fi
