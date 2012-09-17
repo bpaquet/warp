a270fb3c860495e7e5c5188ad9cd42897d95f67c
Bertrand Paquet <bpaquet@octo.com>
2012-09-18 00:22:48 +0200

    Fix #3 Split .node_version in two : .node_version is a user file, .generated_node_version is generated from package. json Add regex check on package.json node version

--- common/node/lib_node.sh
+++ common/node/lib_node.sh
@@ -5,10 +5,17 @@ generate_node_version() {
 load_node_config() {
   if [ -f .node_version ]; then
     LOCAL_NODE_VERSION=`cat .node_version`
+  elif [ -f .generated_node_version ]; then
+    LOCAL_NODE_VERSION=`cat .generated_node_version`
   else
     if [ -f package.json ]; then
-      cat package.json | $WARP_HOME/common/json.sh | grep '\["engines","node"\]' | awk '{print $2}' | perl -pe 's/"//g' > .node_version
-      LOCAL_NODE_VERSION=`cat .node_version`
+      cat package.json | $WARP_HOME/common/json.sh | grep '\["engines","node"\]' | awk '{print $2}' | perl -pe 's/"//g' > .generated_node_version
+      LOCAL_NODE_VERSION=`cat .generated_node_version | grep -e '[[:digit:]]\.[[:digit:]]\.[[:digit:]]'`
+      if [ "$LOCAL_NODE_VERSION" = "" ]; then
+        echo "Wrong node version found in package.json : `cat .generated_node_version`"
+        rm .node_version
+        exit 82
+      fi
     fi
   fi
   if [ -f npm-shrinkwrap.json ]; then
