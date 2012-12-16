3359aa2b1f92ba98ab61850dc740239d4f15aefb
Bertrand Paquet <bpaquet@octo.com>
2012-12-16 15:49:36 +0100

    Dependencies management under centos

--- common/check_dependencies.sh
+++ common/check_dependencies.sh
@@ -10,14 +10,26 @@ TO_BE_INSTALLED=""
 
 for i in $SYS_DEPENDENCIES; do
   echo "Checking system dependency : $i"
-  RES=$(dpkg -l $i | grep "^ii")
-  if [ "$RES" = "" ]; then
-    TO_BE_INSTALLED="$TO_BE_INSTALLED $i"
+  if [ `which dpkg 2> /dev/null` ]; then
+    RES=$(dpkg -l $i | grep "^ii")
+    if [ "$RES" = "" ]; then
+      TO_BE_INSTALLED="$TO_BE_INSTALLED $i"
+    fi
+  else
+    RES=$(rpm -q $i)
+    if [ $? != 0 ]; then
+      TO_BE_INSTALLED="$TO_BE_INSTALLED $i"
+    fi
   fi
 done
 
 if [ "$TO_BE_INSTALLED" != "" ]; then
+  if [ `which dpkg 2> /dev/null` ]; then
+    COMMAND=aptitude
+  else
+    COMMAND=yum
+  fi
   echo "Failed : missing system dependencies. To remove this check, please set the environment variable NO_CHECK_SYS_DEPENDENCIES"
-  echo "Please run : sudo aptitude install${TO_BE_INSTALLED}"
+  echo "Please run : sudo ${COMMAND} install${TO_BE_INSTALLED}"
   exit 1
 fi
--- common/find_dependencies.sh
+++ common/find_dependencies.sh
@@ -2,8 +2,23 @@
 
 TARGET=$1
 
-(
-  find $TARGET -name '*.so' -exec ldd {} \;
-  find $TARGET -executable -type f -exec ldd {} \;
-) | grep '=>' | grep -v 'not found' | awk '{print $3}' | grep -v '^(0x' | sort | uniq | xargs dpkg -S | awk -F: '{print $1}' | sort | uniq | perl -pe 's/\n/ /g'
- 
+if [ `which dpkg 2>&1 2> /dev/null` ]; then
+  (
+    find $TARGET -name '*.so' -exec ldd {} \;
+    find $TARGET -executable -type f -exec ldd {} \;
+  ) | grep '=>' | grep -v 'not found' | awk '{print $3}' | grep -v '^(0x' | sort | uniq | xargs dpkg -S  2> /dev/null | awk -F: '{print $1}' | sort | uniq | perl -pe 's/\n/ /g'
+else
+  LIST=`(
+    find $TARGET -name '*.so' -exec ldd {} \;
+    find $TARGET -executable -type f -exec ldd {} \;
+  ) | grep '=>' | grep -v 'not found' | awk '{print $3}' | grep -v '^(0x' | sort | uniq`
+  TMP_FILE=`mktemp`
+  for i in $LIST; do
+    X=`rpm -q --queryformat '%{NAME}\n' --whatprovides $i`
+    if [ $? = 0 ]; then
+      echo $X >> $TMP_FILE
+    fi
+  done
+  cat $TMP_FILE | sort | uniq | perl -pe 's/\n/ /g'
+  rm $TMP_FILE
+fi
