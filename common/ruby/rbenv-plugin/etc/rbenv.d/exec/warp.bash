83075e2b9af4eed1688b812567d4d67b9525241e
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-10-28 11:19:21 +0100

    Fix some LB_LIBRARY_PATH loading problems

--- common/ruby/rbenv-plugin/etc/rbenv.d/exec/warp.bash
+++ common/ruby/rbenv-plugin/etc/rbenv.d/exec/warp.bash
@@ -1,3 +1,4 @@
+
 BIN_PATH=$RBENV_BIN_PATH
 if [ ! -d $BIN_PATH/../lib ]; then
   BIN_PATH=`cd $BIN_PATH/../../../bin && pwd`
@@ -6,4 +7,5 @@ fi
 export LD_LIBRARY_PATH="$BIN_PATH/../lib:$LD_LIBRARY_PATH"
 if [ -f "$BIN_PATH/.rubylib" ]; then
   export RUBYLIB="`cat $BIN_PATH/.rubylib`:$RUBYLIB"
+  export LD_LIBRARY_PATH="`cat $BIN_PATH/.rubylib`:$LD_LIBRARY_PATH"
 fi
