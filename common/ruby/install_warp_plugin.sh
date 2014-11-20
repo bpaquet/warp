d2fb3bb5d27832c2d53ede605e561d67d29772cf
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-11-20 14:41:12 +0100

    More fix

--- common/ruby/install_warp_plugin.sh
+++ common/ruby/install_warp_plugin.sh
@@ -8,8 +8,13 @@ load_lib ruby
 
 assert_rbenv_installed
 
+# old way
 if [ -d $RBENV_DIR/plugins/warp ]; then
-  run rm -rf $RBENV_DIR/plugins/warp $RBENV_DIR/plugins/00warp
+  run rm -rf $RBENV_DIR/plugins/warp
+fi
+
+if [ -d $RBENV_DIR/plugins/00warp ]; then
+  run rm -rf $RBENV_DIR/plugins/00warp
 fi
 
 run cp -r $WARP_HOME/common/ruby/rbenv-plugin $RBENV_DIR/plugins/00warp
